acaca5d293d027e5668872ab77b3a7610af2f9e6
Lok <137918730+Loksundeep-lab@users.noreply.github.com>
2023-06-28 20:03:39 +0000

    Update two-fer.ts

--- two-fer.ts
+++ two-fer.ts
@@ -5,9 +5,12 @@ export function twoFer(name: string) {
   const str2 = "one for me";
   const str3 = "One for you";
 
-  if (name === "Bob") {
-    return `${str1} ${name}, ${str2}.`;
-  } else if (name === "Alice") {
+  //if (name === "Bob") {
+  //return `${str1} ${name}, ${str2}.`;
+  //} else if (name === "Alice") {
+  // return `${str1} ${name}, ${str2}.`;
+  // } else return `${str3}, ${str2}.`;
+  if (name && name.length > 0) {
     return `${str1} ${name}, ${str2}.`;
   } else return `${str3}, ${str2}.`;
 }
