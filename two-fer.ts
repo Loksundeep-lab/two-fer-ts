6d53d08e0b35defb505a50be54dd15ad9251f517
Lok <137918730+Loksundeep-lab@users.noreply.github.com>
2023-06-28 18:08:54 +0000

    Update 4 files

--- two-fer.ts
+++ two-fer.ts
@@ -1,1 +1,13 @@
-export function twoFer() {}
+import { Console } from "console";
+
+export function twoFer(name: string) {
+  const str1 = "One for";
+  const str2 = "one for me";
+  const str3 = "One for you";
+
+  if (name === "Bob") {
+    return `${str1} ${name}, ${str2}.`;
+  } else if (name === "Alice") {
+    return `${str1} ${name}, ${str2}.`;
+  } else return `${str3}, ${str2}.`;
+}
